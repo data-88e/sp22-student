a76ae41aa3a1f8d99ecac37fab1d8c50ea5e571d
Rohan Jha <rohan.jha@berkeley.edu>
2021-12-28 11:37:26 +0400

    cloned student repo

---
+++ lab/lab04/tests/q2_2.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2_2',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> data.num_rows == 12810\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> len(data.labels) == 52\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(data.column("cgdpe").item(-1), 42325.11719)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
