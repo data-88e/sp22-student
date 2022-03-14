bec6dc27b2ae11782be155e47e4edf502126d72d
Rohan Jha <rohan.jha@berkeley.edu>
2022-03-13 22:20:37 -0700

    added game theory stuff

---
+++ lab/lab06/tests/q2_2.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q2_2',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> assert type(q_1) == sympy.Symbol\n'
+                                               '>>> assert type(q_2) == int\n'
+                                               '>>> assert q_1 in profit_function.as_coefficients_dict()\n'
+                                               '>>> assert len(profit_function.as_coefficients_dict()) == 2\n',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab06/tests/q2_4.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_4',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> assert type(p_1_star) == sympy.core.numbers.Float\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
