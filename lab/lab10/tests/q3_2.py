fd01587e04ff4df7b4e0729a3263c3cc92f8b257
Rohan Jha <rohan.jha@berkeley.edu>
2022-04-24 23:48:36 -0700

    updated stuff

--- lab/lab10/tests/q3_2.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q3_2',
-    'points': None,
-    'suites': [   {   'cases': [{'code': '>>> assert phillips_curve_table.num_rows == 257\n>>> assert phillips_curve_table.num_columns == 3\n', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
