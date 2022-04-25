83c790fde85aeb837ee42dc84336dcb4f4acb81c
Rohan Jha <rohan.jha@berkeley.edu>
2022-04-24 23:20:17 -0700

    things are big confusing

---
+++ lab/lab10/tests/q1_1.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q1_1',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> type(my_api_key) == str\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ lab/lab10/tests/q3_4.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q3_4',
+    'points': [0.25, 0.75, 1.0],
+    'suites': [   {   'cases': [   {'code': '>>> assert callable(find_year_for_date)\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert phillips_curve_table.num_rows == 257\n>>> assert phillips_curve_table.num_columns == 4\n', 'hidden': False, 'locked': False},
+                                   {'code': ">>> assert phillips_curve_table.column('decade')[0] == 1960\n", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
